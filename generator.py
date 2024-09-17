a8fac4f39cab6f4fdcef51dfc01772548a764937
darthvader1925 <elijb12@gmail.com>
2024-09-16 20:40:06 -0700

    semi circle

--- generator.py
+++ generator.py
@@ -16,39 +16,43 @@ def generateImage(shape, character, color):
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype("arial.ttf", 50)
 
+    width, height = 200, 200
+
     if shape == "rectangle":
         width, height = 200, 100
         draw.rectangle([(0, 0), (200, 100)], fill=color)
-        _, _, w, h = draw.textbbox((0, 0), character, font=font)
-        draw.text(((width-w)/2, (height-h)/2), character, font=font, fill="white")
 
     elif shape == "triangle":
         draw.polygon([(100,0), (0,200), (200, 200)], fill=color)
-        _, _, w, h = draw.textbbox((0, 0), character, font=font)
-        draw.text(((200-w)/2, (250-h)/2), character, font=font, fill="white")   
+        width, height = 200, 250
 
     elif shape == "circle":
         draw.ellipse((0, 0, 200, 200), fill=color)
-        _, _, w, h = draw.textbbox((0, 0), character, font=font)
-        draw.text(((200-w)/2, (200-h)/2), character, font=font, fill="white")
+        width, height = 200, 200
 
     elif shape == "quarter circle":
-        draw.pieslice((-50, -50, 200, 200), start=0, end=90, fill=color)
+        draw.pieslice((0, 0, 200, 200), start=0, end=90, fill=color)
         # figure out sizing and text placement later
 
     elif shape == "pentagon":
         draw.regular_polygon((100, 100, 100), 5, fill=color)
-        _, _, w, h = draw.textbbox((0, 0), character, font=font)
-        draw.text(((200-w)/2, (200-h)/2), character, font=font, fill="white")
+        width, height = 200, 200
 
     elif shape == "cross":
-        draw.rectangle([(0, 50), (200, 100)], fill=color)
-        draw.rectangle([(100 / 2, 0), (150, 200)], fill=color)
-        _, _, w, h = draw.textbbox((0, 0), character, font=font)
-        draw.text(((200-w)/2, (200-h)/2), character, font=font, fill="white")
+        draw.rectangle([(0, 75), (200, 125)], fill=color)
+        draw.rectangle([(75, 0), (125, 200)], fill=color)
+        width, height = 200, 200
+
+    elif shape == "semicircle":
+        draw.pieslice((0, 0, 200, 200), start=180, end=360, fill=color)
+        width, height = 200, 100
+
+    _, _, w, h = draw.textbbox((0, 0), character, font=font)
+    draw.text(((width-w)/2, (height-h)/2), character, font=font, fill="white")
+
 
     image.save("vision.png")
     #image.save(f"vision-{shape}.png")
 
 
-generateImage("cross", random.choice(letters), (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256)))
+generateImage("semicircle", random.choice(letters), (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256)))
