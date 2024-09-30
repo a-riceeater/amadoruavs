5d2fab79d7aa5b4d49347ffa4849714e0cccff48
a-riceeater <elijb12@gmail.com>
2024-09-29 18:27:19 -0700

    find center code

--- center.py
+++ center.py
@@ -9,61 +9,75 @@ import geopandas
 
 path = 'with outliers.txt'
 
-coordinates = []
 
-with open(path, 'r') as file:
-    number_of_coordinates = int(file.readline().strip())
+def getCenters(path):
+    coordinates = []
+
+    with open(path, 'r') as file:
+        for line in file:
+            lat, lon = map(float, line.strip().split())
+            coordinates.append((lat, lon))
+
+    X = np.asarray(coordinates)
+    db = DBSCAN(eps=0.00002, min_samples=5)
+    db.fit(X)
+    labels = db.labels_
+    unique_labels = set(labels)
+
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+
+    print("Estimated number of clusters: %d" % n_clusters_)
+    print("Estimated number of noise points: %d" % n_noise_)
+
+    centroids = []
+
+    for k in unique_labels:
+        if k == -1:
+            continue
+        class_member_mask = labels == k
+        cluster_points = X[class_member_mask]
     
-    for line in file:
-        lat, lon = map(float, line.strip().split())
-        coordinates.append((lat, lon))
+        centroid = np.mean(cluster_points, axis=0)
+        centroids.append(centroid)
 
-longi, lat = zip(*coordinates)
-plt.scatter(longi, lat)
-plt.show()
+        plt.plot(centroid[0], centroid[1], 'X', color='red', markersize=15, label=f'Centroid {k}')
 
-X = np.asarray(coordinates)
-db = DBSCAN(eps=0.00002, min_samples=5)
-db.fit(X)
-labels = db.labels_
+    plt.title(f"Estimated number of clusters: {n_clusters_}")
+    plt.show()
 
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-n_noise_ = list(labels).count(-1)
 
-print("Estimated number of clusters: %d" % n_clusters_)
-print("Estimated number of noise points: %d" % n_noise_)
 
-unique_labels = set(labels)
-core_samples_mask = np.zeros_like(labels, dtype=bool)
-core_samples_mask[db.core_sample_indices_] = True
+# optional visual display of plotting
+def display(labels, db):
+    unique_labels = set(labels)
+    core_samples_mask = np.zeros_like(labels, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
 
-colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
 
-    class_member_mask = labels == k
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            col = [0, 0, 0, 1]
 
-    xy = X[class_member_mask & core_samples_mask]
-    plt.plot(
-        xy[:, 0],
-        xy[:, 1],
-        "o",
-        markerfacecolor=tuple(col),
-        markeredgecolor="k",
-        markersize=14,
-    )
+        class_member_mask = labels == k
 
-    xy = X[class_member_mask & ~core_samples_mask]
-    plt.plot(
-        xy[:, 0],
-        xy[:, 1],
-        "o",
-        markerfacecolor=tuple(col),
-        markeredgecolor="k",
-        markersize=6,
-    )
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(
+            xy[:, 0],
+            xy[:, 1],
+            "o",
+            markerfacecolor=tuple(col),
+            markeredgecolor=(1, 1, 1, 0),
+            markersize=6,
+        )
 
-plt.title(f"Estimated number of clusters: {n_clusters_}")
-plt.show()
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(
+            xy[:, 0],
+            xy[:, 1],
+            "o",
+            markerfacecolor=tuple(col),
+            markeredgecolor=(1, 1, 1, 0),
+            markersize=6,
+        )
