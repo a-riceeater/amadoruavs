8c5a80b658f63a7c54244d0b935deeb302153ca5
a-riceeater <elijb12@gmail.com>
2024-09-29 20:21:26 -0700

    coordinates

--- geofence.py
+++ geofence.py
@@ -3,7 +3,17 @@ import numpy as np
 n = 0 # geofence coordinates (amt)
 m = 0 # waypoint coordinates (amt)
 
+fence = [] # fence coordinates
+waypoints = [] # waypoint coordinates
+
 with open("navigate.txt", "r") as file:
     for i, line in enumerate(file):
         if i == 0:
             n, m = map(int, line.split(" "))
+        elif i <= n:
+            lat, lon = map(float, line.strip().split())
+            fence.append((lat, lon))
+        else:
+            lat, lon = map(float, line.strip().split())
+            waypoints.append((lat, lon))
+        
